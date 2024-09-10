0282afff97f55c8804900fa7665e18a369eb1b8b
Juan Diego García <salasjuan@uninorte.edu.co>
2024-09-09 19:40:59 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  path: "https://thehoodini.github.io/3-WEB-FE-REACT-VITE-C1/"
+})
